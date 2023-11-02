04c3b59aea486817abb03a0384f95bd587c747e2
Mudit Pathak <mudit.pathak@coforge.com>
2023-11-02 11:59:16 +0530

    first commit

---
+++ providers.tf
@@ -0,0 +1,12 @@
+# main.tf | Main Configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name" {
+  description = "Unique Name for the Bucket"
+  default     = ""
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-east-2"
+  type        = string
+}
